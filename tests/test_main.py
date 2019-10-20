9647a52613a28ea232c968c7a4fd5ea86d6e61f4
Duncan Parkes <deparkes@gmail.com>
2019-10-20 15:29:46 +0100

    Fix failing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,4 +3,4 @@ from my_package import main
 
 class TestMain(unittest.TestCase):
     def test_main(self):
-        assert main.main() == "Hello, World"
+        assert main.main() == "Hello, World!"
